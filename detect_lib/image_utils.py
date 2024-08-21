0f9c69681f818a4f012d8f8c5a8abe3997e04909
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-08-22 01:59:26 +0800

    using pathlib instead of os.path

--- detect_lib/image_utils.py
+++ detect_lib/image_utils.py
@@ -7,10 +7,17 @@ import threading
 def mutithread_imwrite(imgs:list[np.ndarray], paths:list[os.PathLike], MAX_Thread_num:int=10):
     
     write_times = math.ceil(len(paths)/MAX_Thread_num)
+    
     for i in range(write_times):
         T = list(
-            threading.Thread(target=cv2.imwrite, args=(paths[j], imgs[j])) 
-            for j in range(MAX_Thread_num*i, min(MAX_Thread_num*(i+1), len(paths)))
+            threading.Thread(
+                target=cv2.imwrite, 
+                args=(str(paths[j]), imgs[j])
+            ) 
+            for j in range(
+                MAX_Thread_num*i, 
+                min(MAX_Thread_num*(i+1), len(paths))
+            )
         )
     
         for tid in range(len(T)):
