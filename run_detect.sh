f61e9acdeb46f609348d4ffb1ed93af8e1b9a222
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-08-22 23:29:28 +0800

    sample shell scripts

--- run_detect.sh
+++ run_detect.sh
@@ -1,3 +1,4 @@
+# This is just an example. Please modify it according to your dataset dir
 python detect.py \
     --video_root /mnt/d/AIcitySP8/labeling/dataset \
     --batch_size 180 \
--- run_format_2_mot2d.sh
+++ run_format_2_mot2d.sh
@@ -1,3 +1,5 @@
+# This is just an example. Please modify it according to your dataset dir
+
 python convert_tid.py \
     --staff_annotation_root /mnt/d/AIcitySP8/labeling/dataset/ \
     --to_root /mnt/d/AIcitySP8/labeling/mot2d/ \
