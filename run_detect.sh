9583dd5ddcccc3d8881bf8a6f5b98c3bd5e75424
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-08-22 23:28:13 +0800

    sample script for exe the code

---
+++ run_detect.sh
@@ -0,0 +1,4 @@
+python detect.py \
+    --video_root /mnt/d/AIcitySP8/labeling/dataset \
+    --batch_size 180 \
+    --fps 1
---
+++ run_format_2_yolo.sh
@@ -0,0 +1,4 @@
+python convert_tid.py \
+    --staff_annotation_root /mnt/d/AIcitySP8/labeling/dataset \
+    --to_root /mnt/d/AIcitySP8/labeling/yolo/ \
+    --format yolo
